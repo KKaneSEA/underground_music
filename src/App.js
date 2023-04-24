61b78f97bd622b52a0900d826407a981c5bb544a
K Kane <kane.kayla@gmail.com>
2023-04-23 18:03:59 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="Header">Underground Music</header>
+      <div className="Body_Container">
+        <div className="Canvas"> canvas</div>
+        <div className="Details">details</div>
+      </div>
+      <footer>footer</footer>
     </div>
   );
 }
