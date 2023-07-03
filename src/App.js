f101bc3ba7d789bad89567b3db602644c4e4a08b
K Kane <kane.kayla@gmail.com>
2023-07-02 18:11:28 -0700

    add player functionality

--- src/App.js
+++ src/App.js
@@ -6,39 +6,17 @@ import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import { useAnimations, OrbitControls, Environment } from "@react-three/drei";
 import { useGLTF, CameraShake } from "@react-three/drei";
 import "./styles/App.scss";
-import { Debug, RigidBody, Physics, CuboidCollider } from "@react-three/rapier";
+import {
+  Debug,
+  RigidBody,
+  Physics,
+  CylinderCollider,
+  CuboidCollider,
+} from "@react-three/rapier";
 import useSound from "use-sound";
 import sound1 from "./sounds/sound1.mp3";
 
 let emptyScene = [];
-
-// function Model(positionBox) {
-//   const { scene } = useLoader(GLTFLoader, "./models/undergroundmusic.glb");
-//   console.log(scene);
-//   console.log(scene.children[2]);
-//   emptyScene = scene.children[2];
-//   // emptyScene.rotation.y = 45;
-//   console.log(emptyScene);
-//   return (
-//     <primitive
-//       object={scene}
-//       position={[-29.5, 7.5, 45.9]}
-
-//       // rotation={props.positionBox}
-//     />
-//   );
-// }
-
-// function HeaderText(props) {
-//   return (
-//     <mesh position={props.positionBox}>
-//       <boxBufferGeometry attach="geometry" />
-
-//       {/* <meshLambertMaterial attach="material" color="blue" /> */}
-//       <meshPhongMaterial attach="material" color="green" />
-//     </mesh>
-//   );
-// }
 
 function Env() {
   return <Environment files="./images/OmKHPark_EnvM.hdr" />;
@@ -46,21 +24,12 @@ function Env() {
 
 function App() {
   const [rotateY, setRotateY] = useState(0);
+  const [jumpLeft, setJumpLeft] = useState(0);
+  const [jumpRight, setJumpRight] = useState(0);
   const modelUnderground = useGLTF("./models/otherUndergroundMusic.glb");
   const bricksUnderground = useGLTF("./models/bricksUndergroundMusic.glb");
 
   const brickRef = useRef();
-  const personRef = useRef();
-
-  // const personUnderground = useGLTF("./models/personUndergroundMusic.glb");
-
-  // const animationPerson = useAnimations(
-  //   personUnderground.animations,
-  //   personUnderground.scene
-  // );
-
-  // console.log(personUnderground);
-  // console.log("here");
 
   let sceneRotate = modelUnderground.scene.children[2];
   let bricksRotateY = bricksUnderground.scene.children[2];
@@ -68,63 +37,51 @@ function App() {
   sceneRotate.rotation.y = rotateY;
   bricksRotateY.rotation.y = rotateY;
   brickRef.rotation = { x: 0, y: 0, z: 0 };
-  console.log(brickRef);
 
-  const [upArrowCSS, setUpArrowCSS] = useState("ButtonArrowUp");
-  // const [rotateAxis, setRotateAxis] = useState(45);
-  // const [positionBox, setPositionBox] = useState([1, rotateAxis, 1]);
-  // const [positionBox, setPositionBox] = useState(rotateAxis);
-  // const [keepPositionBox, setKeepPositionBox] = useState();
-  // const gltf = useLoader(GLTFLoader, "./models/undergroundmusic.glb");
   const [play] = useSound(sound1);
 
-  function handleButton(object) {
-    console.log("handled");
+  function handleButtonLeft() {
     play();
+    setJumpLeft(jumpLeft + 1);
+  }
 
-    // setRotateAxis(rotateAxis + 1);
-    // setPositionBox([1, rotateAxis, 1]);
+  function handleButtonRight() {
+    play();
+    setJumpRight(jumpRight + 1);
   }
 
   function handleButtonUp(evt) {
-    console.log("handled up");
-    // setRotateAxis(rotateAxis + 1);
-    // setPositionBox([1, rotateAxis, 1]);
     setRotateY(rotateY - 0.02);
   }
 
   function handleButtonDown(evt) {
-    console.log("handled down");
-    // setRotateAxis(rotateAxis - 1);
-    // setPositionBox([1, rotateAxis, 1]);
     setRotateY(rotateY + 0.02);
   }
 
-  function handleKey(e) {
-    console.log(e.key);
-    let x = e.key;
-    if (
-      x === "ArrowUp" ||
-      x === "ArrowLeft" ||
-      x === "ArrowRight" ||
-      x === "ArrowDown"
-    )
-      // handleButton(e);
-      console.log("enter submitted");
-    console.log(`Button${x}`);
-    // setRotateAxis(rotateAxis + 1);
-    // setPositionBox([1, rotateAxis, 1]);
-    // setUpArrowCSS(`Button${x}:focus`);
-    // console.log(upArrowCSS);
-  }
+  // function handleKey(e) {
+  //   console.log(e.key);
+  //   let x = e.key;
+
+  //   if (x === "ArrowLeft") handleButtonLeft();
+  //   else return;
+  //   // if (
+  //   //   x === "ArrowUp" ||
+  //   //   x === "ArrowLeft" ||
+  //   //   x === "ArrowRight" ||
+  //   //   x === "ArrowDown"
+  //   // )
+  //   // handleButton(e);
+  //   console.log("enter submitted");
+  //   console.log(`Button${x}`);
+  // }
 
   return (
     <div
       className="App"
-      onKeyDown={(e) => {
-        handleKey(e);
-        console.log(e);
-      }}
+      // onKeyDown={(e) => {
+      //   handleKey(e);
+      //   console.log(e);
+      // }}
     >
       <header className="Header">UNDERGROUND MUSIC</header>
       <div className="Body_Container">
@@ -132,41 +89,47 @@ function App() {
           {" "}
           <Canvas
             className="Three_Canvas"
-            camera={{ fov: 85, position: [15.5, 12, 37] }}
+            camera={{ fov: 75, position: [15.5, 12, 37.7] }}
           >
             <directionalLight
-              position={[1, 1, 1]}
+              position={[90, 70, -20]}
               color={"white"}
-              angle={-10.9}
+              angle={-7}
+              intensity={0.8}
             />
-            {/* <OrbitControls /> */}
+
             <Env />
 
-            {/* <spotLight position={[20, 900, -9]} color={"grey"} angle={90.9} /> */}
-
-            {/* <HeaderText positionBox={positionBox} /> */}
-
-            {/* <Model positionBox={positionBox} /> */}
+            {/* <spotLight
+              position={[20, 90, -9]}
+              color={"grey"}
+              angle={900.9}
+              intensity={0.5}
+            /> */}
 
             <Suspense fallback={<Loading position-y={1.0} scale={[5, 5, 5]} />}>
               <Physics>
-                <Debug />
-
-                <RigidBody>
+                <RigidBody type="fixed">
                   <primitive
                     object={modelUnderground.scene}
-                    position={[-29.5, 7.5, 35.9]}
+                    position={[-29.8, 7.5, 35.9]}
                   />
                 </RigidBody>
 
-                <RigidBody type="kinematicPosition" ref={brickRef}>
-                  <primitive
-                    object={bricksUnderground.scene}
-                    position={[-29.5, 7.5, 36.9]}
-                  />
-                </RigidBody>
+                {/* <RigidBody
+                  type="fixed"
+                  ref={brickRef}
+                  // colliders={false}
+                  // onCollisionEnter={collisionEnter}
+                > */}
 
-                <Person handleButton={handleButton} />
+                <primitive
+                  object={bricksUnderground.scene}
+                  position={[-29.5, 7.5, 36.9]}
+                />
+                {/* </RigidBody> */}
+
+                <Person jumpLeft={jumpLeft} jumpRight={jumpRight} />
               </Physics>
             </Suspense>
           </Canvas>
@@ -193,7 +156,7 @@ function App() {
                   <button
                     className="ButtonArrowLeft"
                     onClick={(e) => {
-                      handleButton();
+                      handleButtonLeft();
                     }}
                   >
                     <img
@@ -207,7 +170,7 @@ function App() {
                   <button
                     className="ButtonArrowRight"
                     onClick={(e) => {
-                      handleButton();
+                      handleButtonRight();
                     }}
                   >
                     <img
--- src/Person.js
+++ src/Person.js
@@ -1,51 +1,87 @@
 import * as THREE from "three";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import { useAnimations, OrbitControls, Environment } from "@react-three/drei";
-import { useGLTF, CameraShake } from "@react-three/drei";
+import { useGLTF } from "@react-three/drei";
 import "./styles/App.scss";
 import { useRef, useEffect } from "react";
-import { Debug, RigidBody, Physics } from "@react-three/rapier";
+import { Debug, RigidBody, Physics, CuboidCollider } from "@react-three/rapier";
 
 export default function Person(props) {
   const personUnderground = useGLTF("./models/personUndergroundMusic.glb");
 
   const personRef = useRef();
 
-  // const personJump = () => {
-  //   const mass = personRef.current.mass();
-  //   // console.log(mass);
-  //   // console.log(cube.current);
-  //   personRef.current.applyImpulse({ x: -2, y: 5 * mass, z: 0 });
-  //   personRef.current.applyTorqueImpulse({
-  //     x: Math.random() - 0.5,
-  //     y: 1,
+  // function personJump() {
+  //   // const mass = personRef.current.mass();
+
+  //   // personRef.current.applyImpulse({ x: -20, y: 90, z: 0 });
+  //   console.log("jump");
+  //   console.log(personRef.current);
+
+  //   personRef.current.setTranslation({
+  //     x: 2.5,
+  //     y: (Math.random() + 2.5) * 0.9,
   //     z: 0,
   //   });
-  // };
 
-  function personJump() {
-    // const mass = personRef.current.mass();
+  //   setTimeout(() => {
+  //     personRef.current.setTranslation({ x: 0, y: 0, z: 0 });
+  //   }, 175);
+  // }
 
-    // personRef.current.applyImpulse({ x: -20, y: 90, z: 0 });
-    console.log("jump");
-    console.log(personRef.current);
+  // function personJumpLeft() {
+  //   // const mass = personRef.current.mass();
 
-    // personRef.current.applyTorqueImpulse({
-    //   x: Math.random() - 0.5,
-    //   y: 5,
-    //   z: 0,
-    // });
-  }
+  //   // personRef.current.applyImpulse({ x: -20, y: 90, z: 0 });
+  //   console.log("jump");
+  //   console.log(personRef.current);
 
-  useFrame((state) => {
-    const time = state.clock.getElapsedTime();
+  //   personRef.current.setTranslation({
+  //     x: -2.5,
+  //     y: (Math.random() + 2.5) * 0.9,
+  //     z: 0,
+  //   });
 
-    const eulerRotation = new THREE.Euler(5, time * 6, 0);
+  //   setTimeout(() => {
+  //     personRef.current.setTranslation({ x: 0, y: 0, z: 0 });
+  //   }, 175);
+  // }
 
-    const quaternionRotation = new THREE.Quaternion();
-    quaternionRotation.setFromEuler(eulerRotation);
-    personRef.current.setNextKinematicTranslation(quaternionRotation);
-  });
+  // useFrame((state) => {
+  //   const time = state.clock.getElapsedTime();
+
+  //   const eulerRotation = new THREE.Euler(time * 1, 0, 0);
+
+  //   const quaternionRotation = new THREE.Quaternion();
+  //   quaternionRotation.setFromEuler(eulerRotation);
+  //   personRef.current.setNextKinematicTranslation(quaternionRotation);
+  // });
+
+  useEffect(() => {
+    personRef.current.setTranslation({
+      x: Math.random() - 2.5,
+      // y: (Math.random() + 2.5) * 0.9,
+      y: Math.random() * 5,
+      z: 0,
+    });
+
+    setTimeout(() => {
+      personRef.current.setTranslation({ x: 0, y: 0, z: 0 });
+    }, 175);
+  }, [props.jumpLeft]);
+
+  useEffect(() => {
+    personRef.current.setTranslation({
+      x: Math.random() + 2.5,
+      // y: (Math.random() + 2.5) * 0.9,
+      y: Math.random() * 5,
+      z: 0,
+    });
+
+    setTimeout(() => {
+      personRef.current.setTranslation({ x: 0, y: 0, z: 0 });
+    }, 175);
+  }, [props.jumpRight]);
 
   return (
     <>
@@ -53,8 +89,10 @@ export default function Person(props) {
         canSleep={false}
         type="kinematicPosition"
         ref={personRef}
-        onClick={personJump}
+        gravityScale={1}
+        colliders={false}
       >
+        <CuboidCollider mass={1} args={[8, 7.5, 5]} position={[8, 5, 22]} />
         <primitive
           object={personUnderground.scene}
           position={[-29.5, 7.5, 36.9]}
